185e4f2a69db397d65c082e3de75caebf27420ab
Alex Bellamy <alexjbellamy@gmail.com>
2022-11-05 17:39:10 -0400

    implemented Thought.js

---
+++ models/Thought.js
@@ -0,0 +1,64 @@
+const { Schema, model, Types } = require('mongoose');
+const userSchema = require('./User');
+const {format} = require('date-fns');
+
+// Schema to create a Reaction
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: new Types.ObjectId,
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+            get: (date) => format(date, 'MM/dd/yyyy')
+        },
+    }
+);
+
+// Schema to create a thought
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+            get: (date) => format(date, 'MM/dd/yyyy')
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+          },
+    }
+);
+
+// Virtual 
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return reactions.length;
+});
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
