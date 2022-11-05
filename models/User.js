ef28787fddf64e1d1872dcdd33d07b7869f2160f
Alex Bellamy <alexjbellamy@gmail.com>
2022-11-05 16:39:27 -0400

    implemented User.js

---
+++ models/User.js
@@ -0,0 +1,30 @@
+const { Schema, model } = require('mongoose');
+const thoughtSchema = require('./Thought');
+
+// Schema to create User model
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            validate: {
+                validator: function (v) {
+                    return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(v);
+                }
+            }
+        },
+        thoughts: [{ type: Schema.Types.ObjectId, ref: 'Thought' }],
+        friends: [{type: Schema.Types.ObjectId, ref: 'User'}],
+    }
+);
+
+const User = model('user', userSchema);
+
+module.exports = User;
